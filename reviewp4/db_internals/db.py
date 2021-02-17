881399ae3fae3562078c6c500981eec7fc6306e9
Vladimir Efremov <vefremov@gmail.com>
2021-02-17 16:39:30 +0200

    Added measuring usage times of DB connections

--- reviewp4/db_internals/db.py
+++ reviewp4/db_internals/db.py
@@ -1,6 +1,7 @@
 from threading import BoundedSemaphore
 import logging
 import warnings
+import time
 from .. import settings
 import reviewp4.db_internals.p4db as p4db
 # logging.basicConfig(level=logging.DEBUG)
@@ -24,13 +25,15 @@ class MyConnectionsPool:
     def __init__(self, n_connections):
         self.sema = BoundedSemaphore(n_connections)
         self.connections = set(create_db_connection() for i in range(n_connections))
+        self.start_times = {id(o): time.time() for o in self.connections}
         log.debug('Created connections pool: %d', len(self.connections))
 
     def get_connection(self) -> p4db.P4DBbase:
         log.debug('Getting connection to DB %s as %s', settings.DB_HOST, settings.DB_USER)
         self.sema.acquire()
         c = self.connections.pop()
-        log.debug('Connections left (get): %d', len(self.connections))
+        self.start_times[id(c)] = time.time()
+        log.debug('Connections left (get %d): %d', id(c), len(self.connections))
         
         c.connection.ping(True)
         return c
@@ -38,7 +41,7 @@ class MyConnectionsPool:
     def return_connection(self, c):
         self.connections.add(c)
         self.sema.release()
-        log.debug('Connections left (return): %d', len(self.connections))
+        log.debug('Connections left (return %d): %d, elapsed %s', id(c), len(self.connections), time.time()-self.start_times[id(c)])
 
 pool = MyConnectionsPool(settings.DB_POOL_SZ)
 
