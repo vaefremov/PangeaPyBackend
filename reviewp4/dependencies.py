ffa40288b9c3272f9f67bc152fdc9be9ac44053b
Vladimir Efremov <vefremov@gmail.com>
2021-04-08 21:41:31 +0200

    User name header changed to X-diProjects-User

--- reviewp4/dependencies.py
+++ reviewp4/dependencies.py
@@ -6,11 +6,11 @@ from .db_internals.p4dbexceptions import DBAuthoritiesException
 
 log = logging.getLogger(__name__)
 
-def extract_name_from_header(x_pangea_user: Optional[str] = Header(None)):
-    log.info('Pangea user: %s', x_pangea_user)
-    if x_pangea_user is None:
+def extract_name_from_header(x_diprojects_user: Optional[str] = Header(None)):
+    log.info('Pangea user: %s', x_diprojects_user)
+    if x_diprojects_user is None:
         raise HTTPException(status_code=401, detail='Header bearing user name is required')
-    return x_pangea_user
+    return x_diprojects_user
 
 async def get_connection(user=Depends(extract_name_from_header)):
     with ConnectionContext() as cn:
